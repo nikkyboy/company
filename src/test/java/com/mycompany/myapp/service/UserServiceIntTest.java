28e84e05f246ca66d13dfd30c61068e827a12d99
Nikhil Arora <nikhilarora.hartron@gmail.com>
2018-04-26 14:20:05 +0530

    Initial application generated by JHipster-4.14.1

---
+++ src/test/java/com/mycompany/myapp/service/UserServiceIntTest.java
@@ -0,0 +1,68 @@
+package com.mycompany.myapp.service;
+
+import com.mycompany.myapp.AbstractCassandraTest;
+import com.mycompany.myapp.PractiseApp;
+import com.mycompany.myapp.config.Constants;
+import com.mycompany.myapp.domain.User;
+import com.mycompany.myapp.repository.UserRepository;
+import com.mycompany.myapp.service.dto.UserDTO;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class for the UserResource REST controller.
+ *
+ * @see UserService
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PractiseApp.class)
+public class UserServiceIntTest extends AbstractCassandraTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    private User user;
+
+    @Before
+    public void init() {
+        userRepository.deleteAll();
+        user = new User();
+        user.setId(UUID.randomUUID().toString());
+        user.setLogin("johndoe");
+        user.setPassword(RandomStringUtils.random(60));
+        user.setActivated(true);
+        user.setEmail("johndoe@localhost");
+        user.setFirstName("john");
+        user.setLastName("doe");
+        user.setLangKey("en");
+    }
+
+    @Test
+    public void assertThatAnonymousUserIsNotGet() {
+        user.setLogin(Constants.ANONYMOUS_USER);
+        if (!userRepository.findOneByLogin(Constants.ANONYMOUS_USER).isPresent()) {
+            userRepository.save(user);
+        }
+        final List<UserDTO> allManagedUsers = userService.getAllManagedUsers();
+        assertThat(allManagedUsers.stream()
+            .noneMatch(user -> Constants.ANONYMOUS_USER.equals(user.getLogin())))
+            .isTrue();
+    }
+
+}
